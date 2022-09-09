4e323aae04e0ba506ecb9eeaf83a5541316595d3
hannahyockel <88843058+hannahyockel@users.noreply.github.com>
2022-09-09 13:39:40 -0500

    add model and routes

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
+require("dotenv").config();
 
+const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
-db.on('error', err => console.error(error));
-db.once('open', () => console.log('connected to database'));
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Database"));
 
-app.listen(3001, () => console.log('listening on port 3001'));
+app.use(express.json());
+
+const subscribersRouter = require("./routes/subscribers");
+app.use("/subscribers", subscribersRouter);
+
+app.listen(3001, () => console.log("Server Started"));

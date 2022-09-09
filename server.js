3b7686e88b9098ff966c8e7dea0cdbab055cc018
hannahyockel <88843058+hannahyockel@users.noreply.github.com>
2022-09-09 12:51:33 -0500

    database connection events, database url

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost/subscribers', { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', err => console.error(error));
+db.once('open', () => console.log('connected to database'));
 
 app.listen(3001, () => console.log('listening on port 3001'));
